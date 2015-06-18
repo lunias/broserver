5ba89de452c0830d1f068af23bf20fd0b4212242
Ethan Anderson <ethanaa@gmail.com>
2015-06-18 15:54:22 -0500

    basic sticky footer and some more toolbar work

--- src/main/webapp/scripts/components/navbar/navbar.controller.js
+++ src/main/webapp/scripts/components/navbar/navbar.controller.js
@@ -1,12 +1,41 @@
 'use strict';
 
 angular.module('broserverApp')
-    .controller('NavbarController', function ($scope, $location, $state, Auth, Principal) {
+    .controller('NavbarController', function ($scope, $location, $state, $mdDialog, Auth, Principal) {
+    	
         $scope.isAuthenticated = Principal.isAuthenticated;
         $scope.$state = $state;
-
-        $scope.logout = function () {
-            Auth.logout();
-            $state.go('home');
+        $scope.notificationsEnabled = true;
+        
+        $scope.toggleNotifications = function () {
+        	$scope.notificationsEnabled = !$scope.notificationsEnabled;        	
         };
+        
+        $scope.profile = function () {
+        	$state.go('settings');
+        };
+        
+        $scope.changePassword = function () {
+        	$state.go('password');
+        }
+        
+        $scope.loginOrOut = function () {
+        	
+        	if ($scope.isAuthenticated()) {
+                Auth.logout();
+                $state.go('home');
+        	} else {
+            	$state.go('login');	
+        	}        	
+        };
+        
+        $scope.changeTheme = function () {
+        	$mdDialog.show(
+        	    $mdDialog.alert()
+        	        .title('You clicked!')
+        	        .content('Pick a theme baby!')
+        	        .ok('Nice')
+        	);
+        };
+        
     });
